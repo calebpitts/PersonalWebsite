8da8f6df03b5fa4205a0c33694dcdc083aa53bd9
Caleb Pitts <caleb.pitts@me.com>
2018-10-02 14:52:17 -0700

    fixed footer css resizing

--- interactive-script.js
+++ interactive-script.js
@@ -3,10 +3,10 @@
     $(document).ready(function(){
         $(window).scroll(function(){
             if ($(this).scrollTop() > $(window).height()) {
-                $(".sidenav a").each(function(i) {
-                    $(this).delay(100 * i).fadeIn(500);
-                });
-                // $('.sidenav').fadeIn(500);
+                // $(".sidenav a").each(function(i) {
+                //     $(this).delay(100 * i).fadeIn(500);
+                // });
+                $('.sidenav').fadeIn(500);
             } else {
                 $('.sidenav').fadeOut(500);
             }
