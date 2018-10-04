168f1bf3ae5e10bc77ec1007d0631bf5c5834a04
Caleb Pitts <caleb.pitts@me.com>
2018-10-04 16:41:28 -0700

    added title screen animations and some screen resizing funcitonality

--- interactive-script.js
+++ interactive-script.js
@@ -1,15 +1,23 @@
-
 (function($) {
     $(document).ready(function(){
         $(window).scroll(function(){
             if ($(this).scrollTop() > $(window).height()) {
+                // var element = document.getElementById("empty-scroll-indicator");
+                // console.log(element);
+                // element.classList.add("scroll-indicator");
                 // $(".sidenav a").each(function(i) {
-                //     $(this).delay(100 * i).fadeIn(500);
+                //     $(this).fadeIn(500);
                 // });
                 $('.sidenav').fadeIn(500);
+                $('.scroll-indicator').fadeIn(500);
             } else {
                 $('.sidenav').fadeOut(500);
+                $('.scroll-indicator').fadeOut(500);
             }
         });
     });
 })(jQuery);
+
+$(window).scroll(function(){
+    $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); 
+});
