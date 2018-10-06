82cfde41c3396ab5beedf3db44b7045602c26ed3
Caleb Pitts <caleb.pitts@me.com>
2018-10-05 18:05:13 -0700

    more formatting adjustments

--- interactive-script.js
+++ interactive-script.js
@@ -8,8 +8,10 @@
                 // $(".sidenav a").each(function(i) {
                 //     $(this).fadeIn(500);
                 // });
-                $('.sidenav').fadeIn(500);
-                $('.scroll-indicator').fadeIn(500);
+                if($(window).width() > 576) {
+                    $('.sidenav').fadeIn(500);
+                    $('.scroll-indicator').fadeIn(500);
+                }
             } else {
                 $('.sidenav').fadeOut(500);
                 $('.scroll-indicator').fadeOut(500);
@@ -18,6 +20,33 @@
     });
 })(jQuery);
 
+
+$(window).on('resize', function(){
+    if ($(window).width() < 768) {
+        // $('.moniter').removeClass('limit-container');
+        $('.title-moniter').removeClass('big-title');
+        $('.title-moniter').addClass('small-title');
+    } else {
+        // $('.moniter').addClass('limit-container');
+        $('.title-moniter').removeClass('small-title');
+        $('.title-moniter').addClass('big-title');
+    }
+});
+
 $(window).scroll(function(){
     $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); 
 });
+
+if ($(window).width() < 768) {
+        // $('.moniter').removeClass('limit-container');
+        $('.title-moniter').removeClass('big-title');
+        $('.title-moniter').addClass('small-title');
+    } else {
+        // $('.moniter').addClass('limit-container');
+        $('.title-moniter').removeClass('small-title');
+        $('.title-moniter').addClass('big-title');
+    }
+
+
+
+
